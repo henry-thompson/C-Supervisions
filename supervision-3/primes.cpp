05258b74f013832396e0e19b1755fbc5a3f0c21f
Henry Thompson <henryithompson@btinternet.com>
2017-02-03 00:19:48 +0000

    Correct numbers referred to when outputting the primes

--- supervision-3/primes.cpp
+++ supervision-3/primes.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     std::cout << "1 is prime: " << ht367::IsPrime<1>::ISPRIME << std::endl;
-    std::cout << "1 is prime: " << ht367::IsPrime<2>::ISPRIME << std::endl;
-    std::cout << "1 is prime: " << ht367::IsPrime<3>::ISPRIME << std::endl;
-    std::cout << "1 is prime: " << ht367::IsPrime<811>::ISPRIME << std::endl;
+    std::cout << "2 is prime: " << ht367::IsPrime<2>::ISPRIME << std::endl;
+    std::cout << "3 is prime: " << ht367::IsPrime<3>::ISPRIME << std::endl;
+    std::cout << "811 is prime: " << ht367::IsPrime<811>::ISPRIME << std::endl;
 }
